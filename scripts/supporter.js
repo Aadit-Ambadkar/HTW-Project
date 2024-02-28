7a092f4b4010806d4e3efb6c9c70981334caab41
Aadit Ambadkar <aadit.ambadkar@gmail.com>
2024-02-28 07:43:08 -0800

    Works

--- scripts/supporter.js
+++ scripts/supporter.js
@@ -15,12 +15,13 @@ export function summonHell(player) {
     world.sendMessage(player.dimension.id);
     world.sendMessage(player.location.x + " " + player.location.y + " " + player.location.z);
     if (playerDimension.id === "minecraft:overworld") {
-        return playerDimension.spawnEntity("minecraft:warden", playerLocation);
-    } else if (playerDimension.id === "minecraft:nether") {
-        return playerDimension.spawnEntity("minecraft:wither", playerLocation);
-    } else if (playerDimension.id === "minecraft:end") {
-        return playerDimension.spawnEntity("minecraft:ender_dragon", playerLocation);
-    }
+        playerDimension.spawnEntity("minecraft:warden", playerLocation);
+    } 
+    // else if (playerDimension.id === "minecraft:nether") {
+    //     return playerDimension.spawnEntity("minecraft:wither", playerLocation);
+    // } else if (playerDimension.id === "minecraft:end") {
+    //     return playerDimension.spawnEntity("minecraft:ender_dragon", playerLocation);
+    // }
 }
 
 
