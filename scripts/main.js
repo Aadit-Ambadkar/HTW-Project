83735f8300dbe22dd5a84b51d01eeda8eaeb72ac
donjuanpond <aadiadgaonkar@gmail.com>
2024-03-06 08:21:51 -0800

    Processed all data

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,24 @@
 import { world, system } from "@minecraft/server";
 import { getPlayer, summonHell } from "./supporter";
+import {data} from "./data"
 
 var introMessage = false;
 
 function mainTick() {
     // world.sendMessage("Please Work")
+    
     try {
         if (system.currentTick % 100 === 0) {
             // if (introMessage === false) {
-            var my_name = getPlayer().name;
+            var my_name = "Aadi";
             // write code here to get the player's name
             // getPlayer().sendMessage("Get ready for hell!");
-            world.sendMessage("Hello " + my_name + "! Tick: " + system.currentTick);
+            world.sendMessage("Hello " + data + "! Tick: " + system.currentTick);
+            //world.sendMessage("trivia of the day: "+data)
                 
                 // introMessage = true;
             // }
-            summonHell(getPlayer());
+            //summonHell(getPlayer());
         }
     }
     catch (e) {
